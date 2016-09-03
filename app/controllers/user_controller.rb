088b339c574cfa1edc922fa56e91b3081e3deef5
Víctor Ramírez <vgramirezc@unal.edu.co>
2016-09-03 00:13:06 -0500

    Create Student model

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,17 +6,22 @@ class UserController < ApplicationController
   def create
     @user = User.new(user_params)
     @user.email = @user.email.downcase
+
+    #Se asume que si se pasa la validaciónde user también pasa la de student.
+    #Los atributos cod y semester de student no son obligatorios aún.
     if @user.save
+      @student = Student.create( email: params[:user][:email] )
       redirect_to root_path
     else
       render :new
     end
+
   end
 
   private
 
   def user_params
       params.require(:user).permit(:full_name, :email, :email_confirmation, :password, :password_confirmation)
-    end
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160902175655) do
+ActiveRecord::Schema.define(version: 20160903043101) do
+
+  create_table "students", id: false, force: :cascade do |t|
+    t.string   "email",      null: false
+    t.integer  "cod"
+    t.integer  "semester"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", id: false, force: :cascade do |t|
     t.string   "full_name"
