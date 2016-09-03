a425ab39526e6b04ff8e15e40576b8d39a3ab8a9
Osman Jimenez <odjimenezg@unal.edu.co>
2016-09-02 23:35:25 -0500

    added validations of some tables

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,3 +1,5 @@
 class Teacher < ApplicationRecord
 	self.primary_key = "email"
+	validates :email, uniqueness: { message: "is already taken"}, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+  	validates :email, presence: { message: "is required" }
 end
