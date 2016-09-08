5bbdf1587ef54b134ba2ded8a28ee54f025e10ea
Manuel Vergara <mavd09@hotmail.com>
2016-09-08 00:10:59 -0500

    Add email autocomplete for sign up view

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#index'
   get 'pages/index'
-  devise_for :users, :controllers => { registrations: 'registrations' }
-  resources :user, only: [:new, :create], param: :email
-
-
+  devise_for :users, :controllers => { registrations: 'users/registrations' }
   #Modificar primary kay, en caso de implementar metodos
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
