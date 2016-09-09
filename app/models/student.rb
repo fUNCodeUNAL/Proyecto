0e2ba907f80cae6f05ed51b598b921c1e819e0a8
Alan Navarro <alanjnavarrom@gmail.com>
2016-09-09 14:26:13 -0500

    fixed student email validation

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,7 @@
 class Student < ApplicationRecord
 	has_and_belongs_to_many :groups
 	self.primary_key = "email"
-	validates :email, presence: true, uniqueness: { message: "is already taken"}, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+	validates :email, presence: true, uniqueness: { message: "is already taken"}
 	#validates :cod	, numericality: { only_integer: true, greater_than: 0 }
 	#validates :semester, numericality: { only_integer: true, greater_than: 0 }
 end
