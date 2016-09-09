e4c98c4dd0af82f9f5bcfa85991eab12be5e0165
Manuel Vergara <mavd09@hotmail.com>
2016-09-09 04:45:19 -0500

    Revert "Add email autocomplete for sign up view"
    This reverts commit 5bbdf1587ef54b134ba2ded8a28ee54f025e10ea.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'pages#index'
   get 'pages/index'
-  devise_for :users, :controllers => { registrations: 'users/registrations' }
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  resources :user, only: [:new, :create], param: :email
+
+
   #Modificar primary kay, en caso de implementar metodos
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
