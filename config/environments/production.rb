c0c6058ab0fd2abafc5bf07a8bec4e38e2d29046
Manuel Vergara <mavd09@hotmail.com>
2016-09-09 04:44:57 -0500

    Revert "Revert "Last chance""
    This reverts commit 708ff24d5120c02b3b02c1608b788abcd57ea42a.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,6 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'fun-code.herokuapp.com' }
-  config.action_mailer.perform_deliveries = true
   ActionMailer::Base.smtp_settings = {
       :address                => "smtp.sendgrid.net",
       :port                   => '587',
