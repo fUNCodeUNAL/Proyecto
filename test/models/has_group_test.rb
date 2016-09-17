aa3893ce3ef48046a4d1037ee109334551a5b495
Alan Navarro <alanjnavarrom@gmail.com>
2016-09-17 17:19:16 -0500

    Unit test for every model.

--- test/models/has_group_test.rb
+++ test/models/has_group_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class HasGroupTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save HasGroup without a group_id" do
+    record = HasGroup.new(student_id: students(:one).username)
+    assert_not record.save
+  end
+
+  test "should not save HasGroup without a student_id" do
+    record = HasGroup.new(group_id: groups(:one).id)
+    assert_not record.save
+  end
+
+  test "should save HasGroup without errors" do
+    record = HasGroup.new(group_id: groups(:two).id, student_id: students(:one).username)
+    assert record.save
+  end
 end
