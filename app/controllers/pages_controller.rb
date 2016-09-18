7f5d75d792480897ea3717c45d2ed626c60197fe
Alan Navarro <alanjnavarrom@gmail.com>
2016-09-18 15:34:56 -0500

    User- search engine functionality

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,17 @@
 class PagesController < ApplicationController
   def index
   end
+
   def profile
     @user = User.find_by(username: params[:username])
   	@student = Student.find_by(username: params[:username])
   	if @user == nil or @student == nil
   		render pages_wrong_path
     end
+  end
+  
+  def search
+  	cant = 20
+  	@list = User.where("username LIKE ?", "%#{params[:search]}%").take(cant)
   end
 end
