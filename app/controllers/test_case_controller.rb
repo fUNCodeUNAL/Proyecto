4011a826238b6cb4ebe778ee97627b6933d51997
Víctor Ramírez <vgramirezc@unal.edu.co>
2016-10-20 00:29:01 -0500

    Created page for editing test cases and added functionality in edit problem page for delete and add new test cases

--- app/controllers/test_case_controller.rb
+++ app/controllers/test_case_controller.rb
@@ -1,2 +1,30 @@
+require 'file_manager'
+
 class TestCaseController < ApplicationController
+
+	before_action :authenticate_user!, only: [ :show_test_case, :edit ]
+
+	def show_test_case
+		fileM = FileManager.new
+		@input = fileM.get_data_url( Problem.find(params[:problem_id]).test_cases[ params[:test_idx].to_i ].url_input.url )
+		@output = fileM.get_data_url( Problem.find(params[:problem_id]).test_cases[ params[:test_idx].to_i ].url_output.url )
+		#render :layout => false
+	end
+
+	def edit
+		file = Tempfile.new("tempFile")
+		file.write(params[:input])
+		file.close
+		filepath = file.path
+		Problem.find( params[:problem_id] ).test_cases[ params[:test_idx].to_i ].update( url_input: File.open(filepath) )
+
+		file = Tempfile.new("tempFile")
+		file.write(params[:output])
+		file.close
+		filepath = file.path
+		Problem.find( params[:problem_id] ).test_cases[ params[:test_idx].to_i ].update( url_output: File.open(filepath) )
+		
+		redirect_to Problem.find( params[:problem_id] )
+	end
+
 end
